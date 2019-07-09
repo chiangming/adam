eb49ac1e0396654787f6dc08a3bc43076b31c489
chiangming <1245333655@qq.comn>
2019-07-09 16:54:18 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Demo project for Spring Boot
 
 ## 资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
+
+https://spring.io/guides    
+https://spring.io/guides/gs/serving-web-content/    
+https://v3.bootcss.com/getting-started/ 
+https://developer.github.com/apps/
--- src/main/java/com/code/matt/controller/IndexController.java
+++ src/main/java/com/code/matt/controller/IndexController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Modified: By
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
+    @GetMapping("/")
+    public String index(){ return "index"; }
 }
