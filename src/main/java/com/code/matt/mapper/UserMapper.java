90123761f5d430490a21b0f837a244588625c1c8
chiangming <1245333655@qq.comn>
2019-07-12 15:53:54 +0800

    add check cookies

--- src/main/java/com/code/matt/mapper/UserMapper.java
+++ src/main/java/com/code/matt/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.code.matt.mapper;
 import com.code.matt.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -16,6 +17,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
-    @Select("select * from user")
-    User findByToken(String token);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
