482b9eeb06bc10c62a47176f943729b01690c5dc
chiangming <1245333655@qq.comn>
2019-07-15 14:03:22 +0800

    update question

--- src/main/java/com/code/matt/controller/PublishController.java
+++ src/main/java/com/code/matt/controller/PublishController.java
@@ -1,12 +1,14 @@
 package com.code.matt.controller;
 
-import com.code.matt.mapper.QuestionMapper;
+import com.code.matt.dto.QuestionDTO;
 import com.code.matt.model.Question;
 import com.code.matt.model.User;
+import com.code.matt.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,7 +24,18 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
 
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish() {
@@ -31,9 +44,10 @@ public class PublishController {
 
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam("title") String title,
-            @RequestParam("description") String description,
-            @RequestParam("tag") String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Integer id,
             HttpServletRequest request,
             Model model) {
 
@@ -67,11 +81,9 @@ public class PublishController {
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
+        question.setId(id);
+        questionService.createOrUpdate(question);
 
-
-        questionMapper.create(question);
 
         return "redirect:/";
     }
--- src/main/java/com/code/matt/mapper/QuestionMapper.java
+++ src/main/java/com/code/matt/mapper/QuestionMapper.java
@@ -1,10 +1,7 @@
 package com.code.matt.mapper;
 
 import com.code.matt.model.Question;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -33,4 +30,7 @@ public interface QuestionMapper {
 
     @Select("select * from question where id = #{id}")
     Question getById(@Param(value = "id")Integer id);
+
+    @Update("update question set title = #{title}, description = #{description}, gmt_modify = #{gmtModified}, tag = #{tag} where id = #{id}")
+    void update(Question question);
 }
